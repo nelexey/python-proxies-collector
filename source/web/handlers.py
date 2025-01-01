3b5decc4c4a8dc8688ca95134099764f8a135534
Alexey <codenamed.asyncz@gmail.com>
2025-01-01 21:10:11 +0300

    New functional to /ppxc endpoint
    /ppxc эндпоинт возвращает один актуальный ip прокси.

--- source/web/handlers.py
+++ source/web/handlers.py
@@ -1,16 +1,15 @@
 from aiohttp import web
-import asyncio
 import traceback
 import json
+import aiohttp
 
+from ..collector.checker import fetch
 
 async def ppxc_list(request):
     try:
-        # Read the JSON file
         with open('source/data/proxies_list.json', 'r') as file:
             data = json.load(file)
 
-        # Return the JSON data as a response
         return web.json_response(data)
 
     except Exception as e:
@@ -19,11 +18,16 @@ async def ppxc_list(request):
 
 async def ppxc(request):
     try:
-        return web.Response(text='OK')
+        with open('source/data/proxies_list.json', 'r') as file:
+            data = json.load(file)
+        
+
+        async with aiohttp.ClientSession() as session:
+            for proxy in data['proxies_list']:
+                if await fetch(session, proxy, 'https://api.ipify.org'):
+                    return web.Response(text=proxy, status=200)
+        
+        return web.Response(text="No working proxies available", status=503)
     except Exception as e:
         traceback.print_exc()
         return web.Response(text="Error processing request", status=500)
-
-
-
-
